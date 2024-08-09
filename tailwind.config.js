341af7f3ee7e1fd89bf80c2930bebc97542b8279
martinyde <yde001@gmail.com>
2024-08-09 14:37:29 +0200

    Updated theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: ["./web/themes/custom/giv_din_stemme_theme/**/*.{twig,html,js}"],
   safelist: [
-    'chat-message-container-you',
-    'chat-message-container-bot',
-    'chat-message-container-info',
-    'chat-message-variant-you',
-    'chat-message-variant-bot',
-    'chat-message-variant-info',
-    'chat-message-info-type',
   ],
   theme: {
     extend: {},
