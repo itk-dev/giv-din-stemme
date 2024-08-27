66b85e6f7b396e810ef1dbc14ff66b1338d04375
martinyde <yde001@gmail.com>
2024-08-27 10:23:53 +0200

    Added content pages

--- web/modules/custom/giv_din_stemme/src/Controller/GivDinStemmeController.php
+++ web/modules/custom/giv_din_stemme/src/Controller/GivDinStemmeController.php
@@ -96,6 +96,7 @@ class GivDinStemmeController extends ControllerBase {
   public function consent(Request $request): array {
     return [
       '#theme' => 'consent_page',
+      '#consent_text' => $this->state->get('giv_din_stemme.consent_text'),
     ];
   }
 
@@ -145,6 +146,7 @@ class GivDinStemmeController extends ControllerBase {
   public function permissions(Request $request): array {
     return [
       '#theme' => 'permissions_page',
+      '#permissions_help_page_node' => $this->state->get('giv_din_stemme.permissions_help_page'),
     ];
   }
 
@@ -173,6 +175,7 @@ class GivDinStemmeController extends ControllerBase {
   public function thankYou(Request $request): array {
     return [
       '#theme' => 'thank_you_page',
+      '#thank_you_text' => $this->state->get('giv_din_stemme.thank_you_text'),
     ];
   }
 
