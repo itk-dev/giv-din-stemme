f0be940961d629fb96fd7acba20d90791617656b
jekuaitk <jekua@aarhus.dk>
2024-10-23 10:01:55 +0200

    Made whisper entity data into individual fields

--- web/modules/custom/giv_din_stemme/src/Drush/Commands/GivDinStemmeCommands.php
+++ web/modules/custom/giv_din_stemme/src/Drush/Commands/GivDinStemmeCommands.php
@@ -176,15 +176,12 @@ final class GivDinStemmeCommands extends DrushCommands {
     $whisperGuess = trim(json_decode($response->getBody()->getContents(), TRUE)['text']);
 
     $metadata = $gds->getMetadata();
-
     $originalText = $metadata['text'];
 
     similar_text($originalText, $whisperGuess, $percent);
 
-    $metadata['whisper_guess'] = $whisperGuess;
-    $metadata['whisper_guess_similar_text_score'] = (string) $percent;
-
-    $gds->setMetadata($metadata);
+    $gds->setWhisperGuess($whisperGuess);
+    $gds->setWhisperGuessSimilarTextScore($percent);
 
     // If similar_text score is considered good enough
     // and donation is not validated, validate it.
